43addf3dc5a6c5c372ea65383e3d51285a13232d
Alaeddine <alaeddinegadior@gmail.com>
2026-03-19 01:10:02 +0100

    initial commit

---
+++ driver.py
@@ -0,0 +1,15 @@
+import socket
+
+
+#build a socket
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serversocket.bind((socket.gethostname(), 8000))
+
+
+serversocket.listen(5)
+
+
+
+
