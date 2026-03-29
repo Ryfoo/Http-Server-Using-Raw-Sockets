fd1ec67cf6958391b46b332b53c4d74a0ecb0b8f
Alaeddine <alaxg2005@gmail.com>
2026-03-29 18:33:47 +0100

    minor changes

--- server/server_driver.c
+++ server/server_driver.c
@@ -2,14 +2,29 @@
 #include <stdlib.h>
 #include "include/utils.h"
 #include "include/server.h"
-int main(int args, char* argvs[]) {
-    load_env("server/.env");
+
+
+int main(int argc, char* argv[]) {
+    if(argc < 2)
+    {
+        printf("Usage : %s <running : 0 - for false, 1 - for true>\n", argv[0]);
+        return FAILURE;
+    }
+
+
+    load_env(".env");
     //the main driver resides here!
     char* address = getenv("IP");
     char* port = getenv("PORT");
-    if(server_engine(address, port) < 11) {
+    printf("listening at %s:%s\n", address, port);
+
+    
+    if(server_engine("127.0.0.1", "8080", atoi(argv[1])) != SUCCESS) {
         printf("Server crash\n");
-        return -1;
+        return FAILURE;
     }
-    return 0;
+
+    
+    
+    return SUCCESS;
 }
